9fd42bdad3601c2a8defbf5dd596393b512ac92c
Adam Cox <gadamc@gmail.com>
2012-11-16 18:27:05 +0100

    update

--- rsyncMonitor.sh
+++ rsyncMonitor.sh
@@ -2,7 +2,10 @@
 
 /bin/date
 
-rsync -avz -l --inplace -e '/usr/bin/ssh -p 24' /Users/acquis/Sites/monitoring/ edelweiss@iklx12.fzk.de:/a/iklxserv/users/edelweiss/www/bolomirror
+
+#rsync -avz -l --inplace -e '/usr/bin/ssh -p 24' --delete --delete-excluded --exclude='Runs_*/' /Users/acquis/Sites/monitoring/ edelweiss@iklx12.fzk.de:/a/iklxserv/users/edelweiss/www/bolomirror
+
+rsync -avz -l --inplace -e '/usr/bin/ssh -p 24' --delete --delete-excluded --exclude=".ht*" /Users/acquis/Sites/monitoring/ edelweiss@dm.ikp.kit.edu:/home/edelweiss/www/bolomirror
 
 echo "-----done-----"
 echo ""
